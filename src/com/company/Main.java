440c150b590295e730e64bbf60b17e67a44dd68d
willt2021 <79667361+willt2021@users.noreply.github.com>
2021-10-21 21:19:24 -0700

    First commit of first version.

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    // Not doing a GUI because it is too much...
+    public static void main(String[] args) {
+        // Show the user the database of flights.
+
+        // The user can choose from the selection which one they want to purchase.
+            // Destination, time, price variables must be accounted.
+        
+    }
+}
